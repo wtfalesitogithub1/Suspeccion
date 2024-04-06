a5375cee7d97a348fb1d35efebd0b735320dae02
ALEGAMeRMGITHUB197 <161966183+wtfalesitogithub1@users.noreply.github.com>
2024-04-05 22:31:23 -0400

    Suspeccion
    My ultimate game. Suspeccion.

---
+++ SuspeccionCode.js
@@ -0,0 +1,43 @@
+// Este es el codigo del juego "Suspeccion" no copy paste,
+//                    [CREADO POR]
+//                 @alegamermintheyt 
+//                 @wtfalesito_yt
+//                 @wtfalesitogithub1
+//            Todas esas son cuentas del creador ALEGAMeRMYT197
+// Si no lo conoces:
+//YouTube(Esta el trailer del juego):https://youtube.com/@alegamemintheyt
+//GitHub(Donde descargaste este repositorio)https://github.com/wtfalesitogithub1/
+Gamepad.apply
+scene.Change(PrincipalMenu)//https://scenes.suspe.ccion/ID/809684934
+ scene.Change (Game)//https://scenes.suspe.ccion/ID/79046230934
+KeyboardEvent("Suspeccion")
+console.trace(savana/biome)//https://biomes.suspe.ccion/ID/646889679
+console.error("en/Error:'Please reload the mod or game'es/'Error:Porfavor recarga el mod o el juego'")
+print("ES/Estas entrando al bioma Savana'EN/You its in the Savana biome.")
+sprite.add//https://mobs.suspe.ccion/enemy/ID/780963406
+sprite.add//https://mobs.suspe.ccion/enemy/ID/908563213
+sprite.add//https://mobs.suspe.ccion/enemy/ID/678923462
+sprite.add//https://mobs.suspe.ccion/enemy/ID/325634213
+sprite.add//https://mobs.suspe.ccion/enemy/ID/757473753
+sprite.add//https://mobs.suspe.ccion/boss/ID/574784678
+sprite.damage("damage67'abilitydamage79")//https://mobs.suspe.ccion/enemy/ID/780963406
+sprite.damage("damage47'abilitydamage64")//https://mobs.suspe.ccion/enemy/ID/908563213
+sprite.damage("damage12'abilitydamage36")//https://mobs.suspe.ccion/enemy/ID/678923462
+sprite.damage("damage59'abilitydamage76")//https://mobs.suspe.ccion/enemy/ID/325634213
+sprite.damage("damage27'abilitydamage46")//https://mobs.suspe.ccion/enemy/ID/757473753
+sprite.damage("damage78'abilitydamage98")//https://mobs.suspe.ccion/boss/ID/574784678
+sprite.life("96")//https://mobs.suspe.ccion/enemy/ID/780963406
+sprite.life("79")//https://mobs.suspe.ccion/enemy/ID/908563213
+sprite.life("46")//https://mobs.suspe.ccion/enemy/ID/678923462
+sprite.life("89")//https://mobs.suspe.ccion/enemy/ID/325634213
+sprite.life("74")//https://mobs.suspe.ccion/enemy/ID/757473753
+sprite.life("1027'bossbar/blue'")//https://mobs.suspe.ccion/boss/ID/574784678
+sprite.player.add("link")//https://players.suspe.ccion/ID/568589069
+sprite.player.life("150")//https://players.suspe.ccion/ID/568589069
+sprite.player.damage("50-normal-damage")//https://players.suspe.ccion/ID/568589069
+sprite.player.attack1("damage_78_cooldawn_0.5_70%_probability")("-78")//https://players.suspe.ccion/ID/568589069
+sprite.player.attack2("damage_92_cooldawn_2.5_20%_probability")("Critico! -92")//https://players.suspe.ccion/ID/568589069
+sprite.player.attack3("damage_86_cooldawn_1.5_40%_probability")("Fuerte! -57")//https://players.suspe.ccion/ID/568589069
+sprite.player.ability1("damage_106_reload_48s")//https://players.suspe.ccion/ID/568589069
+sprite.player.ability2("damage_97_reload_36s")//https://players.suspe.ccion/ID/568589069
+sprite.player.ability3("damage_89_reload_24s")//https://players.suspe.ccion/ID/568589069
